38ea8f1473adf885767604aa6a227d105fc3759f
Haluk <haluk.zorluoglu@pointr.tech>
2022-07-20 09:16:47 +0300

    Remove platforms part 2

--- platform/node/test/render.test.js
+++
@@ -1,5 +0,0 @@
-import {run} from '../../../maplibre-gl-js/test/integration/lib/render';
-import implementation from './suite_implementation';
-import ignores from './ignores.json';
-
-run('native', ignores, implementation);
